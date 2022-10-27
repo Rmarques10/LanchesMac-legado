57027677d9131e590a3efa374f5d6ba3e2de5899
Rafael Marques <rafaelmarques.rm86@gmail.com>
2022-10-27 11:39:13 -0300

    First Commit

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,24 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lancheViewModel = new LancheListViewModel();
+            lancheViewModel.Lanches = _lancheRepository.Lanches;
+            lancheViewModel.CategoriaAtual = "Categoria Atual";
+
+            return View(lancheViewModel);
+        }
+    }
+}
